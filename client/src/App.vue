65157e7a88821c89ef5c1154c68b09b8e2c6e379
mschaefer67 <mschaefe@oberlin.edu>
2025-04-13 17:01:59 -0400

    Initial commit client

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="logo" alt="logo" src="./assets/squirrel.jpeg">
+  <MainChat />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MainChat from './components/MainChat.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MainChat
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 300px;
+  height: 300px;
+}
+</style>
---
+++ client/src/components/MainChat.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Welcome to Obiechat</h1>
+    <div>
+        <p>We need to chat brah</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
